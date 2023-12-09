09127dd4715da04f06d99371053050d13b779390
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-12-09 15:12:45 -0300

    Adicionando componnte Menu

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    Default
+    <MenuComponent/>
     <router-view/>
   </div>
 </template>
 
 
 <script>
+import MenuComponent from '@/Components/MenuComponent.vue';
 
 export default {
   name: 'DefaultLayout',
+  components: {
+    MenuComponent
+  }
 }
 </script>
