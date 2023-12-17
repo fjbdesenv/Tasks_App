dcb7aa634b5f90e1913ff32d397dcbb4b3144c76
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-12-17 16:25:12 -0300

    Adicionando mensagem de lista vazia (Tarefas)

--- src/views/tarefa/TarefaListaView.vue
+++ src/views/tarefa/TarefaListaView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <AlertComponent v-if="deletado" tipo="sucesso" texto="Deletado com sucesso!" />
+    <AlertComponent 
+      v-if="mensagem.status" 
+      :tipo="mensagem.tipo" 
+      :texto="mensagem.texto" 
+    />
     <div class="d-flex flex-row flex-wrap">
       <template v-for="(tarefa, index) in listaTarefas" :key="index">
 
@@ -23,7 +27,7 @@
 
 <script>
 import AlertComponent from "@/Components/AlertComponent.vue";
-import CardTarefaComponent from "@/Components/Card/CardTarefaComponent";
+import CardTarefaComponent from "@/Components/Card/CardTarefaComponent.vue";
 import { ApiTask } from "@/Services/ApiTask";
 import { Storage, Sessao, AsycTime } from "@/Utils";
 
@@ -32,8 +36,12 @@ export default {
 
   data: () => ({
     user: undefined,
-    deletado: false,
     listaTarefas: [],
+    mensagem: {
+      status: false,
+      tipo: 'sucesso',
+      texto: ''
+    },
   }),
 
   methods: {
@@ -44,13 +52,8 @@ export default {
         ApiTask.usuario.tarefas.delete(idUser, idTask)
           .then(response =>{
             
+            this.setMensagem('Deletado com sucesso!', 'sucesso', true);
             this.getLista();
-            this.deletado = true;
-            window.location.href = '#top';
-            
-            AsycTime(false, 3000).then(response => {
-              this.deletado = response;
-            });
             
           })
           .catch(error => console.error(error.message));
@@ -61,8 +64,23 @@ export default {
 
       if (this.user) {
         ApiTask.usuario.tarefas.getAll(this.user.id)
-          .then(response => this.listaTarefas = response.data)
+          .then(response => {
+            this.listaTarefas = response.data;
+            if(this.listaTarefas.length == 0) this.setMensagem('Lista está vazia!', 'info', false);
+          })
           .catch(error => console.error(error.message));
+      };
+      
+    },
+
+    setMensagem(texto, tipo, time){
+      window.location.href = '#top';
+      this.mensagem = { status: true, tipo, texto };
+      
+      if(time){
+        AsycTime(false, 3000).then(response => {
+          this.mensagem.status = response;
+        });
       }
     }
   },
@@ -74,7 +92,6 @@ export default {
   beforeMount() {
     this.user = Storage.user || Sessao.user;
     this.getLista();
-
   }
 }
 </script>
