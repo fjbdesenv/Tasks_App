02c42bdf81a13f86e3aaa10c41a238f58cd20f31
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-12-18 12:54:58 -0300

    Adicionando conexão com API para cadastro de tarefa e validação.

--- src/views/tarefa/TarefaCadastroView.vue
+++ src/views/tarefa/TarefaCadastroView.vue
@@ -3,6 +3,7 @@
     <form @submit.prevent="cadastrar()">
 
       <h3 class="text-center mb-4">Cadastro Tarefa</h3>
+      <alert-component v-if="mensagem.show" :tipo="mensagem.tipo" :texto="mensagem.texto"/>
       
       <div class="input-group mb-3">        
         <span class="input-group-text">Título</span>
@@ -41,31 +42,86 @@
 </template>
 
 <script>
+import AlertComponent from '@/Components/AlertComponent.vue';
 import { ApiTask } from '@/Services/ApiTask';
-import { Sessao, Storage } from '@/Utils';
+import { AsycTime, Sessao, Storage } from '@/Utils';
 
 export default {
-  name: 'TarefaCadastroView',
-  
-  data: ()=> ({
-    tarefa:{
-      id_user: 0,
-      titulo: "",
-      descricao: "",
-      status: 1,
-      data: "",
-      horarioInicio: "00:00",
-      horarioFim: "00:00"
-    }
-  }),
-  
-  methods:{
-    cadastrar(){
-      const id_user = Storage.id || Sessao.id;
-      ApiTask.usuario.tarefas.post( id_user ,this.tarefa)
-        .then( response => console.log(response.data))
-        .catch( error => console.error(error))
-    }
-  }
+    name: 'TarefaCadastroView',
+    data: () => ({
+        tarefa: {
+            id_user: 0,
+            titulo: "",
+            descricao: "",
+            status: 1,
+            data: "",
+            horarioInicio: "00:00",
+            horarioFim: "00:00"
+        },
+        mensagem: {
+          show: false,
+          tipo: 'erro',
+          texto: ''
+        }
+    }),
+    methods: {
+        cadastrar() {
+          if(this.validacao()){
+            const id_user = Storage.id || Sessao.id;
+            ApiTask.usuario.tarefas.post(id_user, this.tarefa)
+                .then(response => {
+                  this.mensagemShow('Cadastrado com sucesso!', 'sucesso');
+                  this.limparCampos();
+                })
+                .catch(error => {
+                  console.error(error.message);
+                  this.mensagemErro('Erro inesperado tente novamente mais tarde!', 'erro');
+                });
+          }  
+        },
+        validacao() {
+          let campo = '';
+
+          if (!this.tarefa.titulo)
+            campo = 'Título';
+          else if (!this.tarefa.descricao)
+            campo = 'Descrição';
+          else if (!this.tarefa.data)
+            campo = 'Data';
+          else if (this.tarefa.horarioInicio === '00:00')
+            campo = 'Horário Inicial';
+          else if (this.tarefa.horarioFim === '00:00')
+            campo = 'Horário Final';
+          
+          if(campo) {
+            this.mensagemShow(`Campo ${campo} não foi preenchido`, 'erro');
+            return false;
+          }
+          
+          return true
+
+        },
+
+        mensagemShow(texto, tipo){
+          this.mensagem = {
+            show: true,
+            tipo,
+            texto
+          };
+          AsycTime(false, 3000).then( response => this.mensagem.show = response);
+        },
+        limparCampos(){
+          this.tarefa = {
+            id_user: 0,
+            titulo: "",
+            descricao: "",
+            status: 1,
+            data: "",
+            horarioInicio: "00:00",
+            horarioFim: "00:00"
+          }
+        }
+    },
+    components: { AlertComponent }
 }
 </script>
