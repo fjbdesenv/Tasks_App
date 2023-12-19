df7a4da022877a5d752e7437f323785a84f9e5ae
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-12-19 06:34:44 -0300

    Adicionando lógica de consulta API (edtar-tarefa)

--- src/views/tarefa/TarefaCadastroView.vue
+++ src/views/tarefa/TarefaCadastroView.vue
@@ -1,59 +1,20 @@
 <template>
-  <div>
-    <div class="container">
-      <alert-component v-if="mensagem.show" :tipo="mensagem.tipo" :texto="mensagem.texto" />
-    </div>
-
-    <div class="container border rounded-3 bg-light p-3 mt-3 w-50">
-
-      <form @submit.prevent="cadastrar()">
-
-        <h3 class="text-center mb-4">Cadastro Tarefa</h3>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">Título</span>
-          <input v-model="tarefa.titulo" type="text" aria-label="Titlo" class="form-control">
-        </div>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">Descrição</span>
-          <input v-model="tarefa.descricao" type="text" aria-label="Titlo" class="form-control">
-        </div>
-
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">Data</span>
-          <input v-model="tarefa.data" type="date" aria-label="Data" class="form-control">
-        </div>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">Horário Inícial</span>
-          <input v-model="tarefa.horarioInicio" type="time" aria-label="horario inicial" class="form-control">
-        </div>
-
-        <div class="input-group mb-3">
-          <span class="input-group-text">Horário Final</span>
-          <input v-model="tarefa.horarioFim" type="time" aria-label="horario final" class="form-control">
-        </div>
-
-        <div>
-          <button class="btn btn-success m-2"> Cadastrar </button>
-          <router-link to="/tarefas/lista">
-            <button class="btn btn-danger m-2"> Voltar </button>
-          </router-link>
-        </div>
-      </form>
-    </div>
-  </div>
+  <FormTarefaComponent 
+    :tarefa="tarefa" 
+    :mensagem="mensagem" 
+    operacao="cadastro"
+    @cadastrar="cadastrar()"
+  />
 </template>
 
 <script>
-import AlertComponent from '@/Components/AlertComponent.vue';
 import { ApiTask } from '@/Services/ApiTask';
 import { AsycTime, Sessao, Storage } from '@/Utils';
+import FormTarefaComponent from "@/Components/Form/FormTarefaComponent.vue";
 
 export default {
   name: 'TarefaCadastroView',
+  
   data: () => ({
     tarefa: {
       id_user: 0,
@@ -70,6 +31,10 @@ export default {
       texto: ''
     }
   }),
+  components: {
+    FormTarefaComponent
+  },
+
   methods: {
     cadastrar() {
       if (this.validacao()) {
@@ -123,7 +88,6 @@ export default {
         horarioFim: "00:00"
       }
     }
-  },
-  components: { AlertComponent }
+  }
 }
 </script>
--- src/views/tarefa/TarefaEdicaoView.vue
+++ src/views/tarefa/TarefaEdicaoView.vue
@@ -1,11 +1,104 @@
 <template>
-  <div>
-    Tarefas Edicao
-  </div>
+  <FormTarefaComponent 
+    :tarefa="tarefa" 
+    :mensagem="mensagem" 
+    operacao="edicao"
+    @edicao="edicao()"
+  />
 </template>
 
 <script>
+import { ApiTask } from '@/Services/ApiTask';
+import { AsycTime, Sessao, Storage } from '@/Utils';
+import FormTarefaComponent from "@/Components/Form/FormTarefaComponent.vue";
+
 export default {
   name: 'TarefaEdicaoView',
+
+  data: () => ({
+    tarefa: {
+      _id: 0,
+      id_user: 0,
+      titulo: "",
+      descricao: "",
+      status: 0,
+      data: "",
+      horarioInicio: "00:00",
+      horarioFim: "00:00"
+    },
+    mensagem: {
+      show: false,
+      tipo: 'erro',
+      texto: ''
+    },
+    idUser: 0,
+  }),
+
+  components: {
+    FormTarefaComponent
+  },
+
+  methods: {
+    edicao() {
+      if (this.validacao()) {
+        const idUser = this.idUser;
+        const idTask = this.tarefa._id;
+
+        ApiTask.usuario.tarefas.put(idUser, idTask, this.tarefa)
+          .then(response => {
+            this.showMessage('Editado com sucesso!', 'sucesso');
+          })
+          .catch(error => {
+            console.error(error.message);
+            this.showMessage('Erro inesperado tente novamente mais tarde!', 'erro');
+          });
+      }
+    },
+
+    validacao() {
+      let campo = '';
+
+      if (!this.tarefa.titulo)
+        campo = 'título';
+      else if (!this.tarefa.descricao)
+        campo = 'descrição';
+      else if (!this.tarefa.data)
+        campo = 'data';
+      else if (this.tarefa.status === '0')
+        campo = 'status';
+      else if (this.tarefa.horarioInicio === '00:00')
+        campo = 'horário Inicial';
+      else if (this.tarefa.horarioFim === '00:00')
+        campo = 'horário Final';
+      else return true;
+
+      if (campo) {
+        this.showMessage(`Campo ${campo} não foi preenchido!`, 'erro');
+        return false;
+      }
+    },
+
+    async showMessage(texto, tipo) {
+      this.mensagem = { show: true, tipo, texto };
+      this.mensagem.show = await AsycTime(false);
+    },
+
+    getTarefa(){
+      ApiTask.usuario.tarefas.getId(this.idUser ,this.tarefa._id)
+        .then(response => {
+          this.tarefa = response.data;
+        })
+        .catch(error => {
+          console.error(error.message);
+          this.showMessage('Erro inesperado tente novamente mais tarde!', 'erro');
+        });
+    }
+  },
+
+  beforeMount(){
+    this.idUser = Storage.id || Sessao.id;
+    this.tarefa._id = this.$route.params.id;
+    this.getTarefa();
+  }
 }
 </script>
