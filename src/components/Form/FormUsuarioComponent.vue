800c38bd12aef3116b8f12cd07e5be841aea400b
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2023-12-20 12:26:44 -0300

    Adicionando cadastro de usuário

---
+++ src/components/Form/FormUsuarioComponent.vue
@@ -0,0 +1,116 @@
+<template>
+  <div>
+    <div class="container">
+      <AlertComponent v-if="mensagem.show" :tipo="mensagem.tipo" :texto="mensagem.texto" />
+    </div>
+
+    <div class="container border rounded-3 bg-light p-3 mt-3 w-50">
+
+      <form @submit.prevent="acao()">
+
+        <h3 class="text-center mb-4">{{ textoOperacao }} Usuário</h3>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Nome</span>
+          <input v-model="usuario.nome" type="text" aria-label="Titlo" class="form-control">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">E-mail</span>
+          <input v-model="usuario.email" type="text" aria-label="Titlo" class="form-control">
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">Senha</span>
+          <input v-model="usuario.senha" type="text" autocomplete="off" aria-label="horario inicial" class="form-control">
+        </div>
+        
+        <div class="container mb-3">
+          <div class="form-check">
+            <input v-model="roles.user" class="form-check-input" type="checkbox" id="checkRoleUser">
+            <label class="form-check-label" for="checkRoleUser">
+              Usuário
+            </label>
+          </div>
+          <div class="form-check">
+            <input v-model="roles.adm" class="form-check-input" type="checkbox" id="checkRoleAdm">
+            <label class="form-check-label" for="checkRoleAdm">
+              Administrador
+            </label>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <button class="btn btn-success m-2"> {{ textoOperacao }} </button>
+          <router-link to="/usuarios/lista">
+            <button class="btn btn-danger m-2"> Voltar </button>
+          </router-link>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import AlertComponent from '@/Components/AlertComponent.vue';
+
+export default {
+  name: 'FormUsuarioComponent',
+
+  data: () => ({
+    roles: {
+      user: false,
+      adm: false
+    }
+  }),
+
+  props: {
+    usuario: {
+      type: Object,
+      required: true
+    },
+    mensagem: {
+      type: Object,
+      required: true
+    },
+    operacao: {
+      type: String,
+      required: true,
+      validator(value) {
+        return (value == 'cadastro' || value == 'edicao')
+      }
+    }
+  },
+
+  components: {
+    AlertComponent,
+  },
+
+  computed: {
+    textoOperacao() {
+      if (this.operacao === "cadastro") return "Cadastrar";
+      else if (this.operacao === "edicao") return "Editar";
+    }
+  },
+
+  methods: {
+    acao() {
+      if (this.usuario) {
+        this.usuario.roles = [];
+        if (this.roles.user) this.usuario.roles.push('ROLE_USER');
+        if (this.roles.adm) this.usuario.roles.push('ROLE_ADM');
+      }
+
+      if (this.operacao === "cadastro") this.$emit('cadastrar');
+      else if (this.operacao === "edicao") this.$emit('edicao');
+    }
+  },
+
+  beforeMount() {
+    if (this.usuario) {
+      if (this.usuario.roles.includes('ROLE_USER')) this.roles.user = true;
+      if (this.usuario.roles.includes('ROLE_ADM')) this.roles.adm = true;
+    }
+  }
+};
+</script>
--- src/views/usuario/UsuarioCadastroView.vue
+++ src/views/usuario/UsuarioCadastroView.vue
@@ -1,11 +1,84 @@
 <template>
   <div>
-    Usuarios Cadastro
+    <FormUsuarioComponent
+      :usuario="usuario" 
+      :mensagem="mensagem" 
+      operacao="cadastro"
+      @cadastrar="cadastrar()"
+    />
   </div>
 </template>
 
 <script>
+import FormUsuarioComponent from '@/Components/Form/FormUsuarioComponent.vue';
+import { ApiTask } from '@/Services/ApiTask';
+import { AsycTime, Sessao, Storage } from '@/Utils';
+
 export default {
-  name: 'UsuarioCadastroView',
+    name: 'UsuarioCadastroView',
+    
+    data: ()=>({
+      usuario: {
+        nome: "",
+        email: "",
+        senha: "",
+        roles: []
+      },
+      mensagem: {
+        show: false,
+        tipo: 'erro',
+        texto: ''
+      }
+    }),
+    
+    components: { FormUsuarioComponent },
+
+    methods:{
+      cadastrar() {
+      if (this.validacao()) {
+        const id_user = Storage.id || Sessao.id;
+        ApiTask.usuario.post(this.usuario)
+          .then(response => {
+            this.showMessage('Cadastrado com sucesso!', 'sucesso');
+            this.limparCampos();
+          })
+          .catch(error => {
+            console.error(error.message);
+            this.showMessage('Erro inesperado tente novamente mais tarde!', 'erro');
+          });
+      }
+    },
+    validacao() {
+      let campo = '';
+
+      if (!this.usuario.nome)
+        campo = 'nome';
+      else if (!this.usuario.email)
+        campo = 'e-mail';
+      else if (!this.usuario.senha)
+        campo = 'senha';
+      else return true;
+
+      if (campo) {
+        this.showMessage(`Campo ${campo} não foi preenchido`, 'erro');
+        return false;
+      }
+
+    },
+
+    async showMessage(texto, tipo) {
+      this.mensagem = { show: true, tipo, texto };
+      this.mensagem.show = await AsycTime(false);
+    },
+    
+    limparCampos() {
+      this.usuario = {
+        nome: "",
+        email: "",
+        senha: "",
+        roles: []
+      }
+    },
+  }
 }
 </script>
